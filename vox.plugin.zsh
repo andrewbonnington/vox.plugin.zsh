fee66892eb83ee1dd192cba18192990fba8a0145
Andrew Bonnington <andrewbonnington@users.noreply.github.com>
2015-09-23 22:04:24 +0100

    Add track and artist information

--- vox.plugin.zsh
+++ vox.plugin.zsh
@@ -5,6 +5,38 @@
 #       VERSION:  1.1.0
 # ------------------------------------------------------------------------------
 
+function _now_playing_info() {
+  local info="$(
+    osascript 2>/dev/null <<EOF
+      tell application "VOX"
+        set trackname to track
+        set artistname to artist
+        set albumname to album
+        set state to player state
+      end tell
+
+      if artistname = missing value then
+        set artistname to ""
+      else
+        set artistname to " – " & artistname
+      end if
+
+      if albumname = missing value then
+        set albumname to ""
+      else
+        set albumname to " (" & albumname & ")"
+      end if
+
+      if state is 1 then
+        return "Now Playing: " & trackname & artistname & albumname
+      else
+        return "VOX is currently paused"
+      end if
+EOF
+  )"
+  echo "$info"
+}
+
 function vox() {
   local opt=$1
   case "$opt" in
@@ -41,21 +73,26 @@ function vox() {
           ;;
         esac
       ;;
+    status)
+      _now_playing_info
+      return 0
+      ;;
     -v|--version)
-      print "1.0"
-      return 1
+      print "1.1.0"
+      return 0
       ;;
     ""|-h|--help)
       echo "Usage: vox <option>"
       echo "\t-h, --help\tShow this message, then exit"
       echo "\t-v, --version\tShow version number, then exit"
-      echo "\nOption:"
+      echo "\nOptions:"
       echo "\tlaunch|quit\t\tLaunch or quit VOX"
-      echo "\tplay|pause|resume\t\tPlay or pause the current track"
+      echo "\tplay|pause|resume\tPlay or pause the current track"
       echo "\trewind|forward\t\tSkip back or ahead in the current track"
       echo "\tfastrewind|fastforward\tSkip further back or ahead in the current track"
-      echo "\tnext|previous\tPlay the next or previous track"
+      echo "\tnext|previous\t\tPlay the next or previous track"
       echo "\tvol|volume [up|down]\tIncrease or decrease the volume"
+      echo "\tstatus\t\t\tShow current track details"
       return 0
       ;;
     *)
